5e6cf72c115602b01ed0548e25c6cf999c86159c
isabel <isabel@isabelroses.com>
2024-05-27 18:47:25 +0100

    feat: some cat packages

---
+++ pkgs/meower/package.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  pins,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage {
+  pname = "meower";
+  inherit ((lib.importTOML (pins.meower + "/Cargo.toml")).package) version;
+
+  src = pins.meower;
+  cargoLock.lockFile = (pins.nixpkgs-using + "/Cargo.lock");
+
+  meta = {
+    description = "Helps you generate your meow mrrrp nya";
+    homepage = "https://github.com/Noxyntious/meower";
+    mainProgram = "meower";
+  };
+}
---
+++ pkgs/purr/package.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  stdenv,
+  darwin,
+  pins,
+}:
+rustPlatform.buildRustPackage {
+  pname = "purr";
+  inherit ((lib.importTOML (pins.purr + "/Cargo.toml")).package) version;
+
+  src = pins.purr;
+  cargoLock.lockFile = (pins.purr + "/Cargo.lock");
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs =
+    [ openssl ]
+    ++ lib.optionals stdenv.isDarwin (
+      with darwin.apple_sdk.frameworks;
+      [
+        Security
+        SystemConfiguration
+      ]
+    );
+
+  meta = {
+    description = "Utility commands for managing userstyles";
+    homepage = "https://github.com/uncenter/purr";
+    license = lib.licenses.mit;
+    mainProgram = "purr";
+  };
+}
