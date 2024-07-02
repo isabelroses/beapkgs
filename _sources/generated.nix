1431a2a288c26ef8399ed1af7d79675afb216243
isabelroses <isabelroses@users.noreply.github.com>
2024-07-02 00:35:26 +0000

    chore: update

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -9,7 +9,7 @@
       repo = "bellado";
       rev = "8b9bb4e19b06a35acd8b2d39830c2751246ff189";
       fetchSubmodules = false;
-      sha256 = "sha256-evko1/qE4oRXTMdCOGuwJRUkRm7Sxb5MhQCTLgx5Z+4=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2023-12-16";
   };
@@ -21,7 +21,7 @@
       repo = "blahaj";
       rev = "265163d8a2135a05132db07000a5c6c8a34a762a";
       fetchSubmodules = false;
-      sha256 = "sha256-KJaz9NEWg8Jam4vY7nu9HkxQodKAxfyq6FrmgRrzaHw=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-28";
   };
@@ -33,7 +33,7 @@
       repo = "go-enum";
       rev = "v0.6.0";
       fetchSubmodules = false;
-      sha256 = "sha256-Mt45Qz8l++bvBLKEpbX0m8iTkHDpsZtdYhhHUprQKY8=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
   };
   gomvp = {
@@ -44,7 +44,7 @@
       repo = "gomvp";
       rev = "5b06c041c898822fd72cd245188e51d112adae74";
       fetchSubmodules = false;
-      sha256 = "sha256-dXjI+nItJCAGKxyC9tX11hxWHCP+NgXtTYtm5+6dqDU=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2022-10-25";
   };
@@ -56,7 +56,7 @@
       repo = "helm-ls";
       rev = "v0.0.17";
       fetchSubmodules = false;
-      sha256 = "sha256-c72QFlsCPBW4biTMh1nxQIEkKPjmSmxOD93Kzduswyo=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
   };
   isabelroses-website = {
@@ -67,7 +67,7 @@
       repo = "website";
       rev = "8ae059c8c6438b6e47bbc1e8b4fa13facf924dd1";
       fetchSubmodules = false;
-      sha256 = "sha256-qLRcIgzz2AmzNVShG587NryWcXC8/1yewg1f0Hkj+uA=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-22";
   };
@@ -79,7 +79,7 @@
       repo = "ito";
       rev = "7dfbb2ab5fff469ff7e74b6c03e85afb786d14be";
       fetchSubmodules = false;
-      sha256 = "sha256-k3QCAgtQ5LeXE6a2N3oDIOvGqInBEQTotsdGbbYR2+0=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-10";
   };
@@ -91,7 +91,7 @@
       repo = "izrss";
       rev = "eb3f319fdd0493ddcfba3f0d4b5514c9a3f961e9";
       fetchSubmodules = false;
-      sha256 = "sha256-SuqVnieTlnTODPshj+fVHz8fLvCIwZnIfKgXCFiEUVA=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-19";
   };
@@ -103,7 +103,7 @@
       repo = "jq-lsp";
       rev = "01e96e9809ef775783cefb41a564d78162cece0f";
       fetchSubmodules = false;
-      sha256 = "sha256-c7uK8WPM/h2PLVLFGeN66SztvzjBCgJje7L14+oErVU=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-04-02";
   };
@@ -115,7 +115,7 @@
       repo = "json-to-struct";
       rev = "1f65fffbfdbb2b0e56a3f6f6cd24daad58e90dec";
       fetchSubmodules = false;
-      sha256 = "sha256-l/XLZstZFVnYXRqtng1oiCSvhNAomyeTuI1OtCb8UHo=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2023-06-13";
   };
@@ -127,7 +127,7 @@
       repo = "meower";
       rev = "edc08e7f0f22041d28c70bccd9fd6100e31c1040";
       fetchSubmodules = false;
-      sha256 = "sha256-aM69CmZYVRuhuUnAsjojraiv9VXTvhJxIPohw50Qpv8=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-15";
   };
@@ -139,7 +139,7 @@
       repo = "nixpkgs-using";
       rev = "738ee58509bb74a9b3404dfa71b3ef59784ad3a0";
       fetchSubmodules = false;
-      sha256 = "sha256-asC2csiiHpjujukoX7unHQKguGm6tRqWVbaI4TWEmtA=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-30";
   };
@@ -151,7 +151,7 @@
       repo = "purr";
       rev = "99bfdbe3270aed9c619bb0c021f3a3f92719295f";
       fetchSubmodules = false;
-      sha256 = "sha256-IATclOEV8s3K5bwqIv7Gsm4IqqUGy0eColRpAlvoqfU=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-29";
   };
@@ -163,28 +163,28 @@
       repo = "ringfairy";
       rev = "2e8936af16428efae5123489d6d4b2504a3c11b3";
       fetchSubmodules = false;
-      sha256 = "sha256-4DWQCyVtqMKKgBAOejve9v/en1tfFi6FN3eeuC7B5I0=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-06-29";
   };
   wezterm = {
     pname = "wezterm";
-    version = "552bb1d66b6d0102961974c0da22dc7c69b0cd30";
+    version = "69686f4564ea460111936dee3179ecf8c80e9930";
     src = fetchFromGitHub {
       owner = "wez";
       repo = "wezterm";
-      rev = "552bb1d66b6d0102961974c0da22dc7c69b0cd30";
+      rev = "69686f4564ea460111936dee3179ecf8c80e9930";
       fetchSubmodules = true;
-      sha256 = "sha256-dr/f/zEqgzJ26s2KkiX6/68IU986jAYpeerRMSqejjM=";
+      sha256 = "sha256-9sYfXoAvNHvelI4NEU7L+DV7hfBoOnJnLiRcmxNvjE4=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./wezterm-552bb1d66b6d0102961974c0da22dc7c69b0cd30/Cargo.lock;
+      lockFile = ./wezterm-69686f4564ea460111936dee3179ecf8c80e9930/Cargo.lock;
       outputHashes = {
         "xcb-imdkit-0.3.0" = "sha256-77KaJO+QJWy3tJ9AF1TXKaQHpoVOfGIRqteyqpQaSWo=";
         "sqlite-cache-0.1.3" = "sha256-sBAC8MsQZgH+dcWpoxzq9iw5078vwzCijgyQnMOWIkk=";
       };
     };
-    date = "2024-06-24";
+    date = "2024-07-01";
   };
   zzz = {
     pname = "zzz";
@@ -194,7 +194,7 @@
       repo = "zzz";
       rev = "3429b1f17c6cb755697e0520d98a223789b7c8c1";
       fetchSubmodules = false;
-      sha256 = "sha256-nogVarYxWoSpDQtk1AQnyOrYFGDwjeN/2ybv5aSGwM4=";
+      sha256 = "sha256-OwEHLotL8nzqM71yGVH1+f2a9wcQLXlLywvecGeBhbs=";
     };
     date = "2024-05-30";
   };
