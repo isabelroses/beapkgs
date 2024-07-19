e857ab8b5e245b8a334e100ab22f4caf95450a1c
isabel <isabel@isabelroses.com>
2024-07-19 10:14:38 +0100

    feat: dev script to start http server

--- shell.nix
+++ shell.nix
@@ -1,9 +1,24 @@
 {
-  pkgs ? import <nixpkgs> { },
+  nvfetcher,
+  mkShellNoCC,
+  nixfmt-rfc-style,
+  simple-http-server,
+  writeShellApplication,
 }:
-pkgs.mkShellNoCC {
-  packages = with pkgs; [
+mkShellNoCC {
+  packages = [
     nvfetcher
     nixfmt-rfc-style
+
+    (writeShellApplication {
+      name = "docs";
+      runtimeInputs = [ simple-http-server ];
+
+      text = ''
+        nix build -L .#beapkgs-docs
+
+        simple-http-server -i result
+      '';
+    })
   ];
 }
