bc489edb2186cfa97917f6c1ed246aeabe61921c
isabel <isabel@isabelroses.com>
2024-08-24 13:40:05 +0100

    refactor(wakapi): upstream

--- modules/nixos/wakapi.nix
+++ modules/nixos/wakapi.nix
@@ -1,4 +1,3 @@
-# modified from https://git.winston.sh/winston/deployment-flake/src/branch/main/modules/wakapi.nix
 {
   lib,
   pkgs,
@@ -7,11 +6,12 @@
 }:
 let
   cfg = config.services.wakapi;
-  user = config.users.users.wakapi.name;
-  group = config.users.groups.wakapi.name;
 
   settingsFormat = pkgs.formats.yaml { };
+  settingsFile = settingsFormat.generate "wakapi-settings" cfg.settings;
+
   inherit (lib)
+    getExe
     mkOption
     mkEnableOption
     mkPackageOption
@@ -19,186 +19,23 @@ let
     mkIf
     optional
     mkMerge
-    mkDefault
     singleton
     ;
-
-  settingsFile = settingsFormat.generate "wakapi-settings" cfg.settings;
-
-  userConfig = {
-    users.users.wakapi = {
-      inherit group;
-      createHome = false;
-      isSystemUser = true;
-    };
-    users.groups.wakapi = { };
-  };
-
-  serviceConfig = {
-    systemd.services.wakapi = {
-      description = "Wakapi (self-hosted WakaTime-compatible backend)";
-      wants = [ "network-online.target" ];
-      after = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
-
-      script = ''
-        exec ${pkgs.wakapi}/bin/wakapi -config ${settingsFile}
-      '';
-
-      serviceConfig = {
-        Environment = mkMerge [
-          (mkIf (cfg.passwordSalt != null) "WAKAPI_PASSWORD_SALT=${cfg.passwordSalt}")
-          (mkIf (cfg.smtpPassword != null) "WAKAPI_MAIL_SMTP_PASS=${cfg.smtpPassword}")
-        ];
-        EnvironmentFile = [
-          (optional (cfg.passwordSaltFile != null) cfg.passwordSaltFile)
-          (optional (cfg.smtpPasswordFile != null) cfg.smtpPasswordFile)
-        ];
-
-        User = user;
-        Group = group;
-
-        DynamicUser = true;
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectProc = "invisible";
-        ProtectSystem = "strict";
-        RestrictAddressFamilies = [
-          "AF_INET"
-          "AF_INET6"
-          "AF_UNIX"
-        ];
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        StateDirectoryMode = "0700";
-        Restart = "always";
-      };
-    };
-
-    services.wakapi.settings = {
-      env = lib.mkDefault "production";
-      inherit (cfg) db;
-      server = {
-        inherit (cfg) port;
-      };
-    };
-
-    assertions = [
-      {
-        assertion = cfg.passwordSalt != null || cfg.passwordSaltFile != null;
-        message = "Either `services.wakapi.passwordSalt` or `services.wakapi.passwordSaltFile` must be set.";
-      }
-      {
-        assertion = cfg.smtpPassword != null -> cfg.smtpPasswordFile != null;
-        message = "Both `smtpPassword` or `smtpPasswordFile` should not be set at the same time.";
-      }
-    ];
-  };
-
-  databaseConfig = {
-    services.postgresql = {
-      enable = true;
-      ensureDatabases = singleton cfg.settings.db.name;
-      ensureUsers = singleton {
-        name = cfg.settings.db.user;
-        ensureDBOwnership = true;
-      };
-      authentication = ''
-        host ${cfg.settings.db.name} ${cfg.settings.db.user} 127.0.0.1/32 trust
-      '';
-    };
-
-    services.wakapi.settings.db = {
-      dialect = "postgres";
-    };
-
-    systemd.services.wakapi = {
-      requires = [ "postgresql.service" ];
-      after = [ "postgresql.service" ];
-    };
-  };
-
-  nginxConfig = mkIf cfg.nginx.enable {
-    services.nginx = {
-      enable = true;
-      virtualHosts.${cfg.domain} = {
-        locations."/".proxyPass = "http://127.0.0.1:${toString cfg.port}";
-
-        enableACME = mkDefault true;
-        forceSSL = mkDefault true;
-      };
-    };
-
-    services.wakapi.settings.server = {
-      public_url = mkDefault cfg.domain;
-    };
-  };
 in
 {
   options.services.wakapi = {
     enable = mkEnableOption "Wakapi";
     package = mkPackageOption pkgs "wakapi" { };
 
-    port = mkOption {
-      type = types.int;
-      default = 3000;
+    settings = mkOption {
+      inherit (settingsFormat) type;
+      default = { };
       description = ''
-        The port to serve Wakapi on.
-        This is used to configure nginx.
+        Settings for Wakapi.
+
+        See [config.default.yml](https://github.com/muety/wakapi/blob/master/config.default.yml) for a list of all possible options.
       '';
     };
-    domain = mkOption {
-      type = types.nullOr types.str;
-      default = null;
-      description = ''
-        The FQDN of the domain to serve Wakapi on.
-        This is used to configure nginx.
-      '';
-    };
-
-    db = {
-      host = mkOption {
-        type = types.str;
-        default = "127.0.0.1";
-        description = ''
-          The database host to use for Wakapi.
-        '';
-      };
-      port = mkOption {
-        type = types.int;
-        default = 5432;
-        description = ''
-          The port to use for the database.
-        '';
-      };
-      name = mkOption {
-        type = types.str;
-        default = "wakapi";
-        description = ''
-          The database name to use for Wakapi.
-        '';
-      };
-      user = mkOption {
-        type = types.str;
-        default = "wakapi";
-        description = ''
-          The database user to use for Wakapi.
-        '';
-      };
-      password = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = ''
-          The database password to use for Wakapi.
-        '';
-      };
-    };
-
-    nginx.enable = mkEnableOption "Wakapi Nginx";
 
     passwordSalt = mkOption {
       type = types.nullOr types.str;
@@ -229,22 +66,99 @@ in
         The path to a file containing the password for the smtp mailer used by Wakapi.
       '';
     };
+  };
 
-    settings = mkOption {
-      inherit (settingsFormat) type;
-      default = { };
-      description = ''
-        Settings for Wakapi.
+  config = mkIf cfg.enable {
+    systemd.services.wakapi = {
+      description = "Wakapi (self-hosted WakaTime-compatible backend)";
+      wants = [
+        "network-online.target"
+      ] ++ optional (cfg.settings.db.dialect == "postgres") "postgresql.service";
+      after = [
+        "network-online.target"
+      ] ++ optional (cfg.settings.db.dialect == "postgres") "postgresql.service";
+      wantedBy = [ "multi-user.target" ];
 
-        See [config.default.yml](https://github.com/muety/wakapi/blob/master/config.default.yml) for a list of all possible options.
+      script = ''
+        exec ${getExe cfg.package} -config ${settingsFile}
+      '';
+
+      serviceConfig = {
+        Environment = mkMerge [
+          (mkIf (cfg.passwordSalt != null) "WAKAPI_PASSWORD_SALT=${cfg.passwordSalt}")
+          (mkIf (cfg.smtpPassword != null) "WAKAPI_MAIL_SMTP_PASS=${cfg.smtpPassword}")
+        ];
+        EnvironmentFile = [
+          (optional (cfg.passwordSaltFile != null) cfg.passwordSaltFile)
+          (optional (cfg.smtpPasswordFile != null) cfg.smtpPasswordFile)
+        ];
+
+        User = config.users.users.wakapi.name;
+        Group = config.users.users.wakapi.group;
+
+        DynamicUser = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectProc = "invisible";
+        ProtectSystem = "strict";
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+          "AF_UNIX"
+        ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        StateDirectoryMode = "0700";
+        Restart = "always";
+      };
+    };
+
+    services.wakapi.settings = {
+      env = lib.mkDefault "production";
+    };
+
+    assertions = [
+      {
+        assertion = cfg.passwordSalt != null || cfg.passwordSaltFile != null;
+        message = "Either `services.wakapi.passwordSalt` or `services.wakapi.passwordSaltFile` must be set.";
+      }
+      {
+        assertion = cfg.passwordSalt != null -> cfg.passwordSaltFile != null;
+        message = "Both `services.wakapi.passwordSalt` `services.wakapi.passwordSaltFile` should not be set at the same time.";
+      }
+      {
+        assertion = cfg.smtpPassword != null -> cfg.smtpPasswordFile != null;
+        message = "Both `services.wakapi.smtpPassword` `services.wakapi.smtpPasswordFile` should not be set at the same time.";
+      }
+    ];
+
+    users = {
+      users.wakapi = {
+        group = "wakapi";
+        createHome = false;
+        isSystemUser = true;
+      };
+      groups.wakapi = { };
+    };
+
+    services.postgresql = mkIf (cfg.settings.db.dialect == "postgres") {
+      enable = true;
+
+      ensureDatabases = singleton cfg.settings.db.name;
+      ensureUsers = singleton {
+        name = cfg.settings.db.user;
+        ensureDBOwnership = true;
+      };
+
+      authentication = ''
+        host ${cfg.settings.db.name} ${cfg.settings.db.user} 127.0.0.1/32 trust
       '';
     };
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    userConfig
-    databaseConfig
-    nginxConfig
-    serviceConfig
-  ]);
+  meta.maintainers = with lib.maintainers; [ isabelroses ];
 }
