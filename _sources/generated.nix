de9669c8a5538b4e77493396669fed82b6c5900e
isabelroses <isabelroses@users.noreply.github.com>
2024-11-15 00:43:44 +0000

    chore: update

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -271,22 +271,22 @@
   };
   wezterm = {
     pname = "wezterm";
-    version = "51c794ac500a2033b9dc570c71810518bfa667a9";
+    version = "979df7826965348345a3305ed889a4b9aef838e1";
     src = fetchFromGitHub {
       owner = "wez";
       repo = "wezterm";
-      rev = "51c794ac500a2033b9dc570c71810518bfa667a9";
+      rev = "979df7826965348345a3305ed889a4b9aef838e1";
       fetchSubmodules = true;
-      sha256 = "sha256-AQPpxRj4q5bNx/yTYOJ0OuDYcNVj5DJ4lWIU7iUU3Hk=";
+      sha256 = "sha256-DJhA2T94KtG+Fj7DACTsM5XyZAl58GfN4q1btUjKw9E=";
     };
     cargoLock."Cargo.lock" = {
-      lockFile = ./wezterm-51c794ac500a2033b9dc570c71810518bfa667a9/Cargo.lock;
+      lockFile = ./wezterm-979df7826965348345a3305ed889a4b9aef838e1/Cargo.lock;
       outputHashes = {
         "xcb-imdkit-0.3.0" = "sha256-77KaJO+QJWy3tJ9AF1TXKaQHpoVOfGIRqteyqpQaSWo=";
         "sqlite-cache-0.1.3" = "sha256-sBAC8MsQZgH+dcWpoxzq9iw5078vwzCijgyQnMOWIkk=";
       };
     };
-    date = "2024-11-04";
+    date = "2024-11-14";
   };
   zzz = {
     pname = "zzz";
