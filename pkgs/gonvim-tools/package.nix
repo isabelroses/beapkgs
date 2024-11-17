c9ed1869cdda9b696d7f7aafd91a760922ab776b
isabel <isabel@isabelroses.com>
2024-11-17 17:46:19 +0000

    refactor: breakup gonvim-tools package in hopes it builds

--- pkgs/gonvim-tools/package.nix
+++ pkgs/gonvim-tools/package.nix
@@ -3,39 +3,6 @@
   pkgs,
 }:
 let
-  # nurPkgs =
-  #   builtins.mapAttrs
-  #     (name: content: pkgs.buildGoModule ({ inherit (pins.${name}) pname version src; } // content))
-  #     {
-  #       go-enum = {
-  #         vendorHash = "sha256-YzIVI+PLZt24s/KjTxifWrvjrIU8jLvkC1lgw4yG6cg=";
-  #         meta = {
-  #           description = "An enum generator for go";
-  #           homepage = "https://github.com/abice/go-enum";
-  #           license = lib.licenses.mit;
-  #           platforms = lib.platforms.unix;
-  #         };
-  #       };
-  #       gomvp = {
-  #         vendorHash = null;
-  #         meta = {
-  #           description = "gomvp lets you refactor/rename packages";
-  #           homepage = "https://github.com/abenz1267/gomvp";
-  #           license = lib.licenses.mit;
-  #           platforms = lib.platforms.unix;
-  #         };
-  #       };
-  #       json-to-struct = {
-  #         vendorHash = "sha256-XeqElLWCHqQxWNrbFGB9nBekG9uWDmv1AkGIoHfX1Co=";
-  #         meta = {
-  #           description = "A simple command-line tool for generating to struct definitions from JSON";
-  #           homepage = "https://github.com/tmc/json-to-struct";
-  #           license = lib.licenses.gpl3;
-  #           platforms = lib.platforms.unix;
-  #         };
-  #       };
-  #     };
-
   paths = with pkgs; [
     # comments are binaries provided by the package
     # see https://github.com/ray-x/go.nvim/blob/master/lua/go/install.lua
@@ -56,10 +23,10 @@ let
     richgo
     gotestsum
     mockgen
-    nurPkgs.json-to-struct
-    nurPkgs.gomvp
+    (callPackage ../json-to-struct { })
+    (callPackage ../gomvp { })
     govulncheck
-    nurPkgs.go-enum
+    (callPackage ../go-enum { })
   ];
 in
 pkgs.symlinkJoin {
