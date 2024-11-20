aec7ebb0fa808cf8f1b8ff2b536e5febac688b0a
isabel <isabel@isabelroses.com>
2024-11-20 10:56:05 +0000

    chore: use fetchFromSourcehut

--- pkgs/term2alpha/package.nix
+++ pkgs/term2alpha/package.nix
@@ -1,6 +1,6 @@
 {
   lib,
-  fetchgit,
+  fetchFromSourcehut,
   haskell,
   ...
 }:
@@ -8,10 +8,11 @@ haskell.packages.ghc94.mkDerivation {
   pname = "term2alpha";
   version = "d1d3b51071e099d0473d9ef228e9bb8bf7b016ca";
 
-  src = fetchgit {
-    url = "https://sr.ht/~zethra/term2alpha";
+  src = fetchFromSourcehut {
+    owner = "~zethra";
+    repo = "term2alpha";
     rev = "d1d3b51071e099d0473d9ef228e9bb8bf7b016ca";
-    sha256 = "sha256-TqGryK4a+zizTq/5xS4HYF6JCeRHP27ZVkGJq22bAbk=";
+    hash = "sha256-TqGryK4a+zizTq/5xS4HYF6JCeRHP27ZVkGJq22bAbk=";
   };
 
   isLibrary = false;
