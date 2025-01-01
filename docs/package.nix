108f78b05d7639eb9e44dec79c191a214c886b11
isabel <isabel@isabelroses.com>
2025-01-01 13:05:58 +0000

    refactor: simplify docs build

--- docs/package.nix
+++ docs/package.nix
@@ -1,55 +1,9 @@
 {
-  lib,
   pkgs,
-  nixosOptionsDoc,
   nuscht-search,
   ...
 }:
 let
-  inherit (lib)
-    evalModules
-    scrubDerivations
-    mkForce
-    filterAttrs
-    ;
-
-  mkEval =
-    module:
-    evalModules {
-      modules = [
-        module
-        {
-          _module = {
-            pkgs = mkForce (scrubDerivations "pkgs" pkgs);
-            check = false;
-          };
-        }
-      ];
-
-      specialArgs = {
-        inherit pkgs;
-      };
-    };
-
-  mkDoc =
-    options:
-    let
-      doc = nixosOptionsDoc {
-        options = filterAttrs (n: _: n != "_module") options;
-        documentType = "none";
-      };
-    in
-    "${doc.optionsJSON}/share/doc/nixos/options.json";
-
-  modulesPath = ../modules;
-  nixosEval = mkEval (import (modulesPath + /nixos));
-  darwinEval = mkEval (import (modulesPath + /darwin));
-  hmEval = mkEval (import (modulesPath + /home-manager));
-
-  nixos = mkDoc nixosEval.options;
-  darwin = mkDoc darwinEval.options;
-  hm = mkDoc hmEval.options;
-
   urlPrefix = "https://github.com/isabelroses/beapkgs/blob/main/";
 in
 nuscht-search.mkMultiSearch {
@@ -59,17 +13,26 @@ nuscht-search.mkMultiSearch {
   scopes = [
     {
       name = "NixOS modules";
-      optionsJSON = nixos;
+      modules = [
+        ../modules/nixos
+        { _module.args = { inherit pkgs; }; }
+      ];
       inherit urlPrefix;
     }
     {
       name = "darwin modules";
-      optionsJSON = darwin;
+      modules = [
+        ../modules/darwin
+        { _module.args = { inherit pkgs; }; }
+      ];
       inherit urlPrefix;
     }
     {
       name = "home-manager modules";
-      optionsJSON = hm;
+      modules = [
+        ../modules/home-manager
+        { _module.args = { inherit pkgs; }; }
+      ];
       inherit urlPrefix;
     }
   ];
