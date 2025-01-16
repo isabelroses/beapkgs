65f902276ba6e967fcb263a0ead0629100a865ef
isabelroses <71222764+isabelroses@users.noreply.github.com>
2025-01-16 00:39:37 +0000

    chore: update

--- pkgs/isabelroses-website/package.nix
+++ pkgs/isabelroses-website/package.nix
@@ -13,8 +13,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "isabelroses";
     repo = "website";
-    rev = "29ccd788262ec14b44474bf1afbdcdb03b53c6b3";
-    hash = "sha256-QGIbIhif4n3sG+ygZV90/vjqQnNMrocV2r9TVaLoaq0=";
+    rev = "a85868b3aee101cb3742493626f9b75eb7a4075e";
+    hash = "sha256-ruWwzsOPfUefx65nIveuzsxYeZJgz9nmvExajNX80j0=";
   };
 
   cargoHash = "sha256-IZGAxOErhGByGEVpo3bctxgSQl6AUMfBRtAHp9JFty8=";
